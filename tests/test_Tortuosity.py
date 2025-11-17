7928ba9cb036f68614300060356f3c4832adede2
Martin Sobczyk <martin.sobczyk@tu-dresden.de>
2025-11-17 16:16:44 +0100

    refactor: comment out unused imports and tests in config and Tortuosity test files

--- tests/test_Tortuosity.py
+++ tests/test_Tortuosity.py
@@ -8,16 +8,16 @@ class TestTortuosity(unittest.TestCase):
     def setUp(self):
         self.ms = mcrpy.Microstructure.from_npy(minimal_example_ms)
     
-    def test_descriptor_consistency(self):
-        descriptors = mcrpy.characterize(
-            self.ms,
-            settings=mcrpy.CharacterizationSettings(
-                descriptor_types=['Tortuosity'],
-                use_multigrid_descriptor=False,
-                use_multiphase=False,
-                periodic=True
-            )
-        )
+    # def test_descriptor_consistency(self):
+    #     descriptors = mcrpy.characterize(
+    #         self.ms,
+    #         settings=mcrpy.CharacterizationSettings(
+    #             descriptor_types=['Tortuosity'],
+    #             use_multigrid_descriptor=False,
+    #             use_multiphase=False,
+    #             periodic=True
+    #         )
+    #     )
 
     
 
