4f3f2d1ede6f5d393e8a8c28fdc79c516129c9a5
Martin Sobczyk <martin.sobczyk@tu-dresden.de>
2025-11-17 16:18:10 +0100

    refactor: remove unused config file for Python path adjustments

--- tests/config.py
+++
@@ -1,4 +0,0 @@
-# adding python path to be able to have access to mcrpy package from sibling folder tests
-# import sys
-# import os
-# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
