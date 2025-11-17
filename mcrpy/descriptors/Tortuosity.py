8f4075ae9606ac4643196fa2b8726c19752c0577
Martin Sobczyk <martin.sobczyk@tu-dresden.de>
2025-11-17 08:57:35 +0100

    feat: add Tortuosity descriptor template

---
+++ mcrpy/descriptors/Tortuosity.py
@@ -0,0 +1,37 @@
+"""
+   Copyright 2025 TU Dresden (Martin Sobczyk as Scientific Employee)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+# from __future__ import annotations
+
+import tensorflow as tf
+
+from mcrpy.src import descriptor_factory
+from mcrpy.descriptors.PhaseDescriptor import PhaseDescriptor
+
+
+class Tortuosity(PhaseDescriptor):
+    is_differentiable = False
+
+    @staticmethod
+    def make_singlephase_descriptor(**kwargs) -> callable:
+
+        @tf.function
+        def compute_descriptor(microstructure: tf.Tensor) -> tf.Tensor:
+            return 0 #todo! tf.math.reduce_mean(microstructure)
+        return compute_descriptor
+
+
+def register() -> None:
+    descriptor_factory.register("Tortuosity", Tortuosity)
