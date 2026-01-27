4e406e1ff50f99f29b5e753b1ae1f8307fcae703
Martin Sobczyk <martin.sobczyk@tu-dresden.de>
2026-01-27 11:37:26 +0100

    on the way to make run_test.py working

--- mcrpy/run_test.py
+++ mcrpy/run_test.py
@@ -11,11 +11,13 @@ characterization_settings = mcrpy.CharacterizationSettings(descriptor_types=desc
                                                            full_3d=True,
                                                            use_multigrid_descriptor=False,
                                                            use_multiphase=True,
+                                                           target_folder='results',
                                                            logging_level=logging.WARNING)
 
 print("Load similar 3D microstructure ...")
 ms2D = mcrpy.load("/home/sobczyk/Dokumente/MCRpy/example_microstructures/BlockingLayer_X_2D_20x20.npy")
-ms3D = mcrpy.load("/home/sobczyk/Dokumente/MCRpy/example_microstructures/BlockingLayer_X_20x20x20.npy")
+#ms3D = mcrpy.load("/home/sobczyk/Dokumente/MCRpy/example_microstructures/BlockingLayer_X_20x20x20.npy")
+ms3D = mcrpy.load("/home/sobczyk/Dokumente/MCRpy/example_microstructures/Diag_3x3x3.npy")
 
 ms = ms3D
 
@@ -35,10 +37,12 @@ print(f"characterization: {description3D}")
 reconstruction_settings3D = mcrpy.ReconstructionSettings(descriptor_types=descriptor_types,
                                     use_multiphase=True, max_iter=2,
                                     full_3d=True,
-                                    convergence_data_steps=5000, outfile_data_steps=10,
+                                    convergence_data_steps=1, outfile_data_steps=1,
                                     optimizer_type="GeneticAlgorithm",
                                     #mutation_rule="relaxed_neighbor",
                                     use_multigrid_descriptor=False,
+                                    use_multigrid_reconstruction=False,
+                                    target_folder='results',
                                     logging_level=logging.WARNING)
 
 # init_ms = np.full_like(ms)
