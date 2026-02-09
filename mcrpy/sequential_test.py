6e70c5a15343c2dd62694f4b764e5c76b8c33ff4
Martin Sobczyk <martin.sobczyk@tu-dresden.de>
2026-02-09 15:01:25 +0100

    adding 2D reconstruction, but doenst work yet

--- mcrpy/sequential_test.py
+++ mcrpy/sequential_test.py
@@ -262,130 +262,18 @@ diff_3D_opimizer.reconstruct()
 diff_3D_opimizer.view_convergence_data()
 diff_3D_opimizer.view_result_ms()
 
-# diff_3D_opimizer = MultiStepOptimizer(full_3d=False,
-#                                       goal_ms="/home/sobczyk/Dokumente/MCRpy/example_microstructures/BlockingLayer_X_32x32x32.npy",
-#                                       is_differentiable=True,
-#                                       use_multigrid=False,
-#                                       use_multiphase=True,
-#                                       descriptor_list=desired_descriptor_list,
-#                                       goal_ms_shape=(20,20,20),
-#                                       max_iter=10,
-#                                       population_size=10,
-#                                       verbose=True)
+diff_2D_opimizer = MultiStepOptimizer(full_3d=False,
+                                      goal_ms="/home/sobczyk/Dokumente/MCRpy/example_microstructures/BlockingLayer_X_32x32x32.npy",
+                                      is_differentiable=True,
+                                      use_multigrid=False,
+                                      use_multiphase=True,
+                                      descriptor_list=desired_descriptor_list,
+                                      goal_ms_shape=(32,32,32),
+                                      max_iter=10,
+                                      population_size=10,
+                                      verbose=True)
 
-
-
-
-
-
-# do_paraview_plot = False
-# if do_paraview_plot:
-#     view(ms_to_reconstruct,save_as='MyMicrostructure')
-
-
-
-# ##### Characterization of the MS which should be resembled using all descriptors
-# characterization_settings3D = mcrpy.CharacterizationSettings(descriptor_types=desired_descriptor_list,
-#                                                            full_3d=True,
-#                                                            limit_to=limit_to,
-#                                                            use_multigrid_descriptor=use_multigrid,
-#                                                            use_multiphase=use_multiphase,
-#                                                            target_folder='results',
-#                                                            logging_level=logging.WARNING)
-
-
-# ##### Characterization of the MS which should be resembled using all descriptors
-# characterization_settings2D = mcrpy.CharacterizationSettings(descriptor_types=desired_differentiable_descriptor_list2D,
-#                                                            full_3d=False,
-#                                                            limit_to=limit_to,
-#                                                            use_multigrid_descriptor=use_multigrid,
-#                                                            use_multiphase=use_multiphase,
-#                                                            target_folder='results',
-#                                                            logging_level=logging.WARNING)
-
-# print(f"Characterize 3D image of shape {ms_to_reconstruct_shape}...")
-# description2D = mcrpy.characterize(ms_to_reconstruct, characterization_settings2D)
-# print("="*60)
-# print(f"characterization: {description2D}")
-
-
-# print(f"Characterize 3D image of shape {ms_to_reconstruct_shape}...")
-# description3D = mcrpy.characterize(ms_to_reconstruct, characterization_settings)
-# print("="*60)
-# print(f"characterization: {description3D}")
-
-
-##### Reconstruction using differential descriptors in 2D and using and initial MS as starting point:
-# reconstruction_settings2D_differentiable = mcrpy.ReconstructionSettings(
-#             descriptor_types=desired_differentiable_descriptor_list2D,
-#             use_multiphase=use_multiphase, 
-#             max_iter=100,
-#             full_3d=False,
-#             limit_to=limit_to,
-#             convergence_data_steps=1, outfile_data_steps=1,
-#             optimizer_type="LBFGSB",
-#             #optimizer_type="SimulatedAnnealing",
-#             use_multigrid_descriptor=use_multigrid,
-#             use_multigrid_reconstruction=use_multigrid,
-#             target_folder='results',
-#             population_size=100,
-#             tolerance=1e-4,
-#             logging_level=logging.INFO)
-
-# if initial_microstructure:
-#     print(f"Reconstruct microstructure with shape {initial_microstructure.spatial_shape}...")
-# else:
-#     print(f"Reconstruct microstructure with shape {ms_to_reconstruct.spatial_shape}...")
-
-# convergence_data2D, ms_reconstruct2D = mcrpy.reconstruct(description2D, ms_to_reconstruct_shape, 
-#                                           settings=reconstruction_settings2D_differentiable,
-#                                           initial_microstructure=initial_microstructure
-#                                           )
-# view(convergence_data2D)
-
-
-
-
-# convergence_data3D, ms_reconstruct3D = mcrpy.reconstruct(description3D, ms_to_reconstruct_shape, 
-#                                           settings=reconstruction_settings3D_differentiable,
-#                                           initial_microstructure=initial_microstructure
-#                                           )
-
-
-
-# reconstruction_settings3D = mcrpy.ReconstructionSettings(descriptor_types=desired_descriptor_list,
-#                                     use_multiphase=use_multiphase, 
-#                                     max_iter=100,
-#                                     full_3d=True,
-#                                     limit_to=limit_to,
-#                                     convergence_data_steps=1, outfile_data_steps=1,
-#                                     #optimizer_type="GeneticAlgorithm",
-#                                     optimizer_type="SimulatedAnnealing",
-#                                     use_multigrid_descriptor=use_multigrid,
-#                                     use_multigrid_reconstruction=use_multigrid,
-#                                     target_folder='results',
-#                                     population_size=100,
-#                                     tolerance=1e-4,
-#                                     logging_level=logging.INFO)
-
-
-
-# print("="*60)
-# if initial_microstructure:
-#     print(f"Reconstruct microstructure with shape {initial_microstructure.spatial_shape}...")
-# convergence_data3D, ms_reconstruct3D = mcrpy.reconstruct(description3D, ms_to_reconstruct_shape, 
-#                                           settings=reconstruction_settings3D,
-#                                           initial_microstructure=initial_microstructure
-#                                           )
-
-# view(convergence_data3D)
-
-# # view(ms_reconstruct3D, )
-# print("characterization of reconstructed ms:")
-# ms_result = convergence_data3D['raw_data'][-1]
-# description3D = mcrpy.characterize(ms_result, characterization_settings)
-# print("="*60)
-# print(f"characterization: {description3D}")
-
-# logging.info("="*60)
-# logging.shutdown() 
+diff_2D_opimizer.get_characterization_goal(verbose=True)
+diff_2D_opimizer.reconstruct()
+diff_2D_opimizer.view_convergence_data()
+diff_2D_opimizer.view_result_ms()
